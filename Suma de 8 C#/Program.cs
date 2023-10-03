07bffd717c0d992435f446f880f9eef1e83ba908
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2023-10-03 12:46:44 -0600

    Agregar archivos de proyecto.

---
+++ Suma de 8 C#/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Practica_01
+{
+    class Program
+    {
+        const int NUM = 8;
+        static void Main()
+        {
+            int[] nums = new int[NUM];
+            int total = 0;
+            for (int i = 0; i < NUM; i++)
+            {
+                Console.Write("Por favor, introduzca el numero: ");
+                nums[i] = int.Parse(Console.ReadLine());
+                total += nums[i];
+
+            }
+            Console.WriteLine("El total de numero es: " + total);
+            Console.ReadKey();
+        }
+    }
+}
